449a42328590fffc0edec07cd5d9607c3587d271
WSX <1040985551@qq.com>
2019-07-29 19:50:30 +0800

    emmm

---
+++ shop_web/src/main/java/com/czxy/MyApplication.java
@@ -0,0 +1,16 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author
+ * @Version v 1.0
+ * @Date 2019/7/29
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class,args);
+    }
+}
